e6f283f2f789b22c455c77fe37de4aee5b396c0e
Marvin <marvin7@stud.fh-dortmund.de>
2017-09-30 15:01:59 +0200

    New dependencies and beginning of storylist

--- erasmus-stories/src/app/story-list/story-list.component.ts
+++ erasmus-stories/src/app/story-list/story-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import _ from 'lodash';
+const MOCK_STORIES: Object[]=[{
+  id: _.uniqueId('story_'),
+  authorID: _.uniqueId('user_'),
+  text: "test story, mega Awesome"
+}];
 @Component({
   selector: 'app-story-list',
   templateUrl: './story-list.component.html',
   styleUrls: ['./story-list.component.css']
 })
 export class StoryListComponent implements OnInit {
-
+  title = "New Stories";
+  stories = MOCK_STORIES;
   constructor() { }
 
   ngOnInit() {
