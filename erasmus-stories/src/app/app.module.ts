472dc90eadaa0ddfbd2217bf23ab4edad70f28b8
StephanieHohenberg <hohenberg_s@web.de>
2017-09-30 12:56:14 +0200

    generating components

--- erasmus-stories/src/app/app.module.ts
+++ erasmus-stories/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { HeaderComponent } from './header/header.component';
+import { StoryListComponent } from './story-list/story-list.component';
+import { NameGameComponent } from './name-game/name-game.component';
+import { FlipCardComponent } from './flip-card/flip-card.component';
+import { ChatComponent } from './chat/chat.component';
+import { StoryComponent } from './story/story.component';
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    HeaderComponent,
+    StoryListComponent,
+    NameGameComponent,
+    FlipCardComponent,
+    ChatComponent,
+    StoryComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ erasmus-stories/src/app/name-game/name-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-game',
+  templateUrl: './name-game.component.html',
+  styleUrls: ['./name-game.component.css']
+})
+export class NameGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
