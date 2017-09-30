6db53496f951cb64bff261b10dad365d7ea27dad
StephanieHohenberg <hohenberg_s@web.de>
2017-09-30 14:46:34 +0200

    routing

--- erasmus-stories/src/app/app.module.ts
+++ erasmus-stories/src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { HeaderComponent } from './components/header/header.component';
-import { StoryListComponent } from './components/story-list/story-list.component';
-import { NameGameComponent } from './components/name-game/name-game.component';
-import { FlipCardComponent } from './components/flip-card/flip-card.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { StoryComponent } from './components/story/story.component';
+import {AppComponent} from "./app.component";
+import {LogInComponent} from "./components/log-in/log-in.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {StoryListComponent} from "./components/story-list/story-list.component";
+import {NameGameComponent} from "./components/name-game/name-game.component";
+import {FlipCardComponent} from "./components/flip-card/flip-card.component";
+import {ChatComponent} from "./components/chat/chat.component";
+import {StoryComponent} from "./components/story/story.component";
 import {SignUpComponent} from "./components/sign-up/sign-up.component";
 import {AngularFireModule} from "angularfire2";
 import {environment} from "../environments/environment";
 import {AngularFireAuthModule} from "angularfire2/auth";
 import {AngularFireDatabaseModule} from "angularfire2/database";
-import {APP_ROUTES} from "./app.routes";
+import {RouterModule} from "@angular/router";
+import {routing} from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -33,11 +34,12 @@ import {APP_ROUTES} from "./app.routes";
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     HttpModule,
-    APP_ROUTES
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- erasmus-stories/src/app/app.routes.ts
+++ erasmus-stories/src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import {Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {StoryListComponent} from "./components/story-list/story-list.component";
 import {LogInComponent} from "./components/log-in/log-in.component";
 import {NameGameComponent} from "./components/name-game/name-game.component";
 
-export const APP_ROUTES : Routes = [
+const APP_ROUTES: Routes = [
   {path:'', component:StoryListComponent},
   {path:'login', component:LogInComponent},
   {path:'game', component:NameGameComponent}
 ];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
